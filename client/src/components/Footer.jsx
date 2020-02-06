c4cf0814eb0aeec1cdab46b2a926d497f33ba3fa
Anthony Jung <junganthonyy@gmail.com>
2020-02-05 20:47:15 -0800

    Complete React Skeleton

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import Feedback from './Feedback.jsx';
 import Email from './Email.jsx';
 import Engage from './Engage.jsx';
 import Links from './Links.jsx';
+import Contact from './Contact.jsx';
 
 class Footer extends React.Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class Footer extends React.Component {
         <div className="AJbottomfooter">
           <Engage></Engage>
           <Links></Links>
+          <Contact></Contact>
         </div>
       </div>
     )
