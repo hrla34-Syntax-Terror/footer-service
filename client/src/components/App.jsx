457c64ead1c184639c05a7f03fa3e4f3a272965c
Anthony Jung <junganthonyy@gmail.com>
2020-02-05 17:37:11 -0800

    Start Setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './Footer.jsx';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <Footer></Footer>
+    );
+  }
+}
+
+export default App;
